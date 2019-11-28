80f1b1158269fd124582c2847da5b05c9dac914e
Nana Yin <yinnana@colostate.edu>
2019-11-27 19:36:30 -0700

    add playGame/invite buttons in frontend

--- PartyA/src/com/partyA/control/MoveServlet.java
+++ PartyA/src/com/partyA/control/MoveServlet.java
@@ -1,8 +1,10 @@
 package com.partyA.control;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,37 +14,46 @@ import com.partyA.bean.Match;
 import com.partyA.bean.User;
 import org.json.JSONObject;
 
+@WebServlet("/move")
 public class MoveServlet extends HttpServlet {
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-
-        String requestUrl = request.getRequestURI();
-        String move = requestUrl.substring("PartyA_war_exploded/move/".length());
-        User user1 = new User();
-        User user2 = new User();
-        GameBoard board = new GameBoard(new Match(user1, user2));
-        if(true){
-            String json = "{\n";
-            json += "\"move\": " + JSONObject.quote(board.toString()) + ",\n";
-            json += "}";
-            response.getOutputStream().println(json);
-        }
-        else{
-            //Return empty JSON object
-            response.getOutputStream().println("{}");
-        }
+        doPost(request,response);
+//        String requestUrl = request.getRequestURI();
+//        String move = requestUrl.substring("PartyA_war_exploded/move/".length());
+//        User user1 = new User();
+//        User user2 = new User();
+//        GameBoard board = new GameBoard(new Match(user1, user2));
+//        if(true){
+//            String json = "{\n";
+//            json += "\"move\": " + JSONObject.quote(board.toString()) + ",\n";
+//            json += "}";
+//            response.getOutputStream().println(json);
+//        }
+//        else{
+//            //Return empty JSON object
+//            response.getOutputStream().println("{}");
+//        }
     }
 
 
 
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        //String name = request.getParameter("user");
-        //String match = request.getParameter("match");
-        System.out.println(request.getPathInfo());
-        String move = request.getPathInfo();
-        System.out.println("Move: " + move);
+        response.setContentType("text/html;charset=utf-8");
+        PrintWriter out = response.getWriter();
+
+        String fromX    = request.getParameter("fromX");
+        String fromY  = request.getParameter("fromY");
+        String toX = request.getParameter("toX");
+        String toY = request.getParameter("toY");
+
+        System.out.println("Move: fromX=" + fromX+"，fromY="+fromY+"，toX="+toX+"，toY="+toY);
+
+        out.write("okay!!");
+        out.flush();
+        out.close();
 
     }
 }
--- PartyA/web/index.jsp
+++ PartyA/web/index.jsp
@@ -1,16 +1,27 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: 李洁琼
-  Date: 2019/11/1
-  Time: 15:33
-  To change this template use File | Settings | File Templates.
---%>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page language="java" pageEncoding="UTF-8"%>
+<%@ page contentType="text/html;charset=UTF-8"%>
+<%
+    String path = request.getContextPath();
+    String basePath = request.getScheme() + "://"
+            + request.getServerName() + ":" + request.getServerPort()
+            + path + "/";
+%>
 <html>
   <head>
     <title>$Title$</title>
+      <script type="text/javascript">
+          function playGame(){
+              alert("you are going to play game.");
+              window.location.href="<%=basePath %>game2.jsp";
+          }
+          function invite(){
+              alert("you are going to invite others.");
+          }
+      </script>
   </head>
   <body>
- partyA project
+  <h1>partyA project</h1>
+  <button id="invite" onclick="invite();">invite others</button>
+ <button id="playGame" onclick="playGame();">play game</button>
   </body>
 </html>
