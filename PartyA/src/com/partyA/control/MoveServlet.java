19dcc04f5bb722adb4b1447454ed972553c1307a
ebeaney <42565452+ebeaney@users.noreply.github.com>
2019-11-29 16:26:30 -0700

    Update MoveServlet.java

--- PartyA/src/com/partyA/control/MoveServlet.java
+++ PartyA/src/com/partyA/control/MoveServlet.java
@@ -1,40 +1,37 @@
 package com.partyA.control;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import com.partyA.bean.GameBoard;
+import com.partyA.bean.Match;
+import com.partyA.bean.MoveConverter;
+import com.partyA.bean.User;
+import com.partyA.exception.IllegalMoveException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.partyA.bean.GameBoard;
-import com.partyA.bean.Match;
-import com.partyA.bean.User;
-import org.json.JSONObject;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/move")
 public class MoveServlet extends HttpServlet {
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        doPost(request,response);
-//        String requestUrl = request.getRequestURI();
-//        String move = requestUrl.substring("PartyA_war_exploded/move/".length());
-//        User user1 = new User();
-//        User user2 = new User();
-//        GameBoard board = new GameBoard(new Match(user1, user2));
-//        if(true){
-//            String json = "{\n";
-//            json += "\"move\": " + JSONObject.quote(board.toString()) + ",\n";
-//            json += "}";
-//            response.getOutputStream().println(json);
-//        }
-//        else{
-//            //Return empty JSON object
-//            response.getOutputStream().println("{}");
-//        }
+        //set to application/json later
+        response.setContentType("text/html;charset=utf-8");
+        PrintWriter out = response.getWriter();
+
+        String fromX    = request.getParameter("fromX");
+        String fromY  = request.getParameter("fromY");
+        String toX = request.getParameter("toX");
+        String toY = request.getParameter("toY");
+        System.out.println("Move: fromX=" + fromX+"，fromY="+fromY+"，toX="+toX+"，toY="+toY);
+
+        out.write("okay!!");
+        out.flush();
+        out.close();
     }
 
 
@@ -48,10 +45,21 @@ public class MoveServlet extends HttpServlet {
         String fromY  = request.getParameter("fromY");
         String toX = request.getParameter("toX");
         String toY = request.getParameter("toY");
-
         System.out.println("Move: fromX=" + fromX+"，fromY="+fromY+"，toX="+toX+"，toY="+toY);
-
-        out.write("okay!!");
+        //Temp initialization to get communication working
+        User black = new User(1, "AAA", "abcx", "AAA@gamil.com");
+        User white = new User(2, "BBB", "fgfd", "BBB@gamil.com");
+        Match m = new Match(black, white);
+        GameBoard board = new GameBoard(m);
+        MoveConverter convert = new MoveConverter();
+        board.initialize();
+        int result =0;
+        try {
+            result = board.move(convert.toBackend(Integer.parseInt(fromX),Integer.parseInt(fromY)), convert.toBackend(Integer.parseInt(toX), Integer.parseInt(toY)));
+        } catch (IllegalMoveException e) {
+            e.printStackTrace();
+        }
+        out.write("" +result);
         out.flush();
         out.close();
 
