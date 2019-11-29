5c183fa60f2469c2924adde0a249a64f2f18b769
JacindaQiong <2568732852@qq.com>
2019-11-29 12:06:29 -0700

    need to bebug

---
+++ PartyA/src/com/partyA/control/SelectUserServlet.java
@@ -0,0 +1,52 @@
+package com.partyA.control;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.partyA.service.UserService;
+
+
+
+/**
+ * Servlet implementation class SelectUserServlet
+ */
+@WebServlet("/back/selectUser")
+public class SelectUserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SelectUserServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String page=request.getParameter("page");
+		String rows=request.getParameter("rows");
+		PrintWriter out=response.getWriter();
+		UserService service =new UserService();
+		String temp=service.searchUser(Integer.parseInt(page), Integer.parseInt(rows));
+		out.print(333);
+	    out.print(temp);
+	    out.flush();
+	}
+
+}
