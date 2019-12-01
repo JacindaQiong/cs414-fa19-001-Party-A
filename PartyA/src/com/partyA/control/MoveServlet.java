8cf1e8ed26e8b3046b819c44e35ac0bfd9c99366
ebeaney <42565452+ebeaney@users.noreply.github.com>
2019-12-01 01:48:29 -0700

    Update MoveServlet.java

--- PartyA/src/com/partyA/control/MoveServlet.java
+++ PartyA/src/com/partyA/control/MoveServlet.java
@@ -5,7 +5,10 @@ import com.partyA.bean.Match;
 import com.partyA.bean.MoveConverter;
 import com.partyA.bean.User;
 import com.partyA.exception.IllegalMoveException;
+import com.partyA.exception.IllegalPositionException;
 
+import javax.json.Json;
+import javax.json.JsonObject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,9 +16,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 @WebServlet("/move")
 public class MoveServlet extends HttpServlet {
+    //Temp initialization to get communication working
+    User black = new User(1, "AAA", "abcx", "AAA@gamil.com");
+    User white = new User(2, "BBB", "fgfd", "BBB@gamil.com");
+    Match m = new Match(black, white);
+    GameBoard board = new GameBoard(m);
+    MoveConverter convert = new MoveConverter();
+
+    public void init() throws ServletException {
+        board.initialize();
+    }
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
@@ -23,11 +37,11 @@ public class MoveServlet extends HttpServlet {
         response.setContentType("text/html;charset=utf-8");
         PrintWriter out = response.getWriter();
 
-        String fromX    = request.getParameter("fromX");
-        String fromY  = request.getParameter("fromY");
+        String fromX = request.getParameter("fromX");
+        String fromY = request.getParameter("fromY");
         String toX = request.getParameter("toX");
         String toY = request.getParameter("toY");
-        System.out.println("Move: fromX=" + fromX+"，fromY="+fromY+"，toX="+toX+"，toY="+toY);
+        System.out.println("Move: fromX=" + fromX + "，fromY=" + fromY + "，toX=" + toX + "，toY=" + toY);
 
         out.write("okay!!");
         out.flush();
@@ -35,31 +49,47 @@ public class MoveServlet extends HttpServlet {
     }
 
 
-
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        response.setContentType("text/html;charset=utf-8");
+        response.setContentType("application/json;charset=utf-8");
         PrintWriter out = response.getWriter();
-
-        String fromX    = request.getParameter("fromX");
-        String fromY  = request.getParameter("fromY");
+        String fromX = request.getParameter("fromX");
+        String fromY = request.getParameter("fromY");
         String toX = request.getParameter("toX");
         String toY = request.getParameter("toY");
-        System.out.println("Move: fromX=" + fromX+"，fromY="+fromY+"，toX="+toX+"，toY="+toY);
-        //Temp initialization to get communication working
-        User black = new User(1, "AAA", "abcx", "AAA@gamil.com");
-        User white = new User(2, "BBB", "fgfd", "BBB@gamil.com");
-        Match m = new Match(black, white);
-        GameBoard board = new GameBoard(m);
-        MoveConverter convert = new MoveConverter();
-        board.initialize();
-        int result =0;
+        System.out.println("Move: fromX=" + fromX + "，fromY=" + fromY + "，toX=" + toX + "，toY=" + toY);
+        int status = 0;
+        String result = "";
+        ArrayList<String> boardArray = new ArrayList<String>();
+        String tempBoard = "";
+        int isLegal = 0;
         try {
-            result = board.move(convert.toBackend(Integer.parseInt(fromX),Integer.parseInt(fromY)), convert.toBackend(Integer.parseInt(toX), Integer.parseInt(toY)));
-        } catch (IllegalMoveException e) {
+            if (board.checkIsLegal(convert.toBackend(Integer.parseInt(fromX), Integer.parseInt(fromY)), convert.toBackend(Integer.parseInt(toX), Integer.parseInt(toY))) == 1) {
+                isLegal = 1;
+                status = board.move(convert.toBackend(Integer.parseInt(fromX), Integer.parseInt(fromY)), convert.toBackend(Integer.parseInt(toX), Integer.parseInt(toY)));
+                System.out.println(board.toString());
+            }
+            boardArray = board.getPieceLocations();
+            for (int i = 0; i < boardArray.size(); i++) {
+                tempBoard += boardArray.get(i);
+            }
+        } catch (IllegalMoveException | IllegalPositionException e) {
             e.printStackTrace();
         }
-        out.write("" +result);
+        //Convert tempBoard to board that gets returned to client
+        for (int i = 0; i < tempBoard.length(); i += 3) {
+            String temp = "";
+            temp += "['" + tempBoard.charAt(i + 0) + "' , ";
+            temp += convert.toFrontEndCol(tempBoard.charAt(i + 1)) + " , ";
+            temp += convert.toFrontEndRow(tempBoard.charAt(i + 2)) + "] ";
+            result += temp;
+        }
+        JsonObject output = Json.createObjectBuilder()
+                .add("legal",isLegal)
+                .add("status", status)
+                .add("board", result)
+                .build();
+        out.write("" + output);
         out.flush();
         out.close();
 
