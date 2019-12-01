da96157a9a1ae88528c5ce54b678c11d893ca7f8
ebeaney <42565452+ebeaney@users.noreply.github.com>
2019-12-01 01:48:55 -0700

    Update MoveConverter.java

--- PartyA/src/com/partyA/bean/MoveConverter.java
+++ PartyA/src/com/partyA/bean/MoveConverter.java
@@ -52,4 +52,92 @@ public class MoveConverter {
         return backendCall;
     }
 
+    public String toFrontEndCol(char col){
+        int temp;
+        switch(col){
+            case 'a':
+                temp = 50;
+                break;
+            case 'b':
+                temp = 100;
+                break;
+            case 'c':
+                temp = 150;
+                break;
+            case 'd':
+                temp = 200;
+                break;
+            case 'e':
+                temp = 250;
+                break;
+            case 'f':
+                temp = 300;
+                break;
+            case 'g':
+                temp = 350;
+                break;
+            case 'h':
+                temp = 400;
+                break;
+            case 'i':
+                temp = 450;
+                break;
+            case 'j':
+                temp = 500;
+                break;
+            case 'k':
+                temp = 550;
+                break;
+            default:
+                temp = -1;
+                break;
+        }
+        String result = "" +temp;
+        return result;
+    }
+    public String toFrontEndRow(char row){
+        int temp;
+        switch(row){
+            case 'a':
+                temp = 550;
+                break;
+            case 'b':
+                temp = 500;
+                break;
+            case 'c':
+                temp = 450;
+                break;
+            case 'd':
+                temp = 400;
+                break;
+            case 'e':
+                temp = 350;
+                break;
+            case 'f':
+                temp = 300;
+                break;
+            case 'g':
+                temp = 250;
+                break;
+            case 'h':
+                temp = 200;
+                break;
+            case 'i':
+                temp = 150;
+                break;
+            case 'j':
+                temp = 100;
+                break;
+            case 'k':
+                temp = 50;
+                break;
+            default:
+                temp = -1;
+                break;
+        }
+        String result = "" +temp;
+        return result;
+
+    }
+
 }
