3be361d644f0fe6fc848aec50d54ddd2260a5cb9
Nana Yin <yinnana@colostate.edu>
2019-12-08 22:12:09 -0700

    update game_invitation SQL

--- SQL/game_invitation.sql
+++ SQL/game_invitation.sql
@@ -1,19 +1,21 @@
 /*
-Navicat MySQL Data Transfer
+ Navicat Premium Data Transfer
 
-Source Server         : 127.0.0.2_3306
-Source Server Version : 50528
-Source Host           : localhost:3306
-Source Database       : partya
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 80018
+ Source Host           : localhost:3306
+ Source Schema         : mysql
 
-Target Server Type    : MYSQL
-Target Server Version : 50528
-File Encoding         : 65001
+ Target Server Type    : MySQL
+ Target Server Version : 80018
+ File Encoding         : 65001
 
-Date: 2019-12-07 20:50:18
+ Date: 08/12/2019 22:11:10
 */
 
-SET FOREIGN_KEY_CHECKS=0;
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
 -- Table structure for game_invitation
@@ -25,11 +27,4 @@ CREATE TABLE `game_invitation` (
   `invitation_time` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of game_invitation
--- ----------------------------
-INSERT INTO `game_invitation` VALUES ('123', '222', '2019-11-20 01:22:30');
-INSERT INTO `game_invitation` VALUES ('1', '2', '3');
-INSERT INTO `game_invitation` VALUES ('123', '123', '123');
-INSERT INTO `game_invitation` VALUES ('eee', '11', '11');
-INSERT INTO `game_invitation` VALUES ('', '', '');
+SET FOREIGN_KEY_CHECKS = 1;
